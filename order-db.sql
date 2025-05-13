b55b046c12e1a3c08121802d443c7b132ff7ad0a
iamjoydevdas <iamjoydevdas@gmail.com>
2025-05-13 23:41:07 +0100

    order

---
+++ order-db.sql
@@ -0,0 +1,22 @@
+DROP SCHEMA order;
+
+CREATE SCHEMA IF NOT EXISTS order;
+
+DROP TABLE IF EXISTS order.orders;
+
+CREATE TABLE order.orders (
+                        id SERIAL PRIMARY KEY,
+                        receiver_name VARCHAR(100) NOT NULL,
+                        receiver_contact VARCHAR(20) NOT NULL,
+                        sender_name VARCHAR(100) NOT NULL,
+                        sender_contact VARCHAR(20) NOT NULL,
+                        sender_email VARCHAR(100),
+                        distance DECIMAL(10, 2), -- in kilometers
+                        price DECIMAL(10, 2),    -- total delivery price
+                        item_code VARCHAR(50) NOT NULL,
+                        height DECIMAL(10, 2),   -- in cm
+                        weight DECIMAL(10, 2),   -- in kg
+                        source_postcode VARCHAR(20) NOT NULL,
+                        destination_postcode VARCHAR(20) NOT NULL,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
